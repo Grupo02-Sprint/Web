cd8a62e9291d960d4c54014e5eef19a70c586ebd
pedrohmunizs <pedro.silva032@sptech.school>
2023-04-30 23:24:51 -0300

    listagem dos funcionarios cadastrados criada

--- site/src/controllers/funcionarioController.js
+++ site/src/controllers/funcionarioController.js
@@ -27,6 +27,22 @@ function cadastrar(req, res) {
             );
     }
 }
+
+function listar(req, res) {
+    funcionarioModel.listar().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os funcionáriso: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 module.exports = {
-    cadastrar
+    cadastrar,
+    listar
 }
--- site/src/routes/funcionarios.js
+++ site/src/routes/funcionarios.js
@@ -7,4 +7,8 @@ router.post("/cadastrar/:idUsuario", function (req, res) {
     funcionarioController.cadastrar(req, res);
 });
 
+router.get("/listar", function (req, res) {
+    funcionarioController.listar(req, res);
+});
+
 module.exports = router;
